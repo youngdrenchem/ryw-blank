f0d3934448c18192aea552bfebedffa4066c73e7
youngdrenchem <mlharris18@cps.edu>
2024-12-09 14:56:25 +0000

    v1

--- script.js
+++ script.js
@@ -12,6 +12,10 @@ let es7 = document.querySelector('.es7');
 es7.style.display="none";
 let nw6 = document.querySelector('.nw6');
 nw6.style.display="none";
+let aj2 = document.querySelector('.aj2');
+aj2.style.display="none";
+let ss2 =document.querySelector('.ss2');
+ss2.style.display="none";
 // left left left left left left left left left left left left
 left.addEventListener('click', function() {
     
@@ -30,7 +34,7 @@ rw2.addEventListener('click', function() {
     right.style.display="none";
     rw2.style.display="none";
     js4.style.display="block";
- text.innerHTML="you ended up giving him the ball and he scored"
+ text.innerHTML="you ended up giving him the ball and he scored";
 });
 rw2.addEventListener('wheel', function() {
     leftRight.style.display="none;"
@@ -48,8 +52,33 @@ rw2.addEventListener('wheel', function() {
     left.style.display="none";
     right.style.display="none";
     nw6.style.display="block";
+    text.innerHTML="you have a double option to give to ashton or noah"
  
     
+   });
 
+   nw6.addEventListener('wheel', function() {
     
- });
+      leftRight.style.display ="none";
+      left.style.display="none";
+      right.style.display="none";
+      aj2.style.display="block";
+      nw6.style.display="none";
+      text.innerHTML="you gave it to ashton and he got you to the 5 yard line now finish it with shedeur"
+
+   
+      
+     });
+     aj2.addEventListener('dblclick', function() {
+    
+      leftRight.style.display ="none";
+      left.style.display="none";
+      right.style.display="none";
+      aj2.style.display="none";
+      nw6.style.display="none";
+      ss2.style.display="block";
+      text.innerHTML=" yayyyy shedeur scored";
+
+   
+      
+     });
